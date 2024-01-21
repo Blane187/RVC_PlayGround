c022a0d78acd5d81f9b07e0116aa0e31ee6522d9
Luis Santillan <Rejekts@users.noreply.huggingface.co>
2024-01-21 09:06:19 +0000

    Update infer/modules/vc/modules.py

--- infer/modules/vc/modules.py
+++ infer/modules/vc/modules.py
@@ -101,8 +101,7 @@ class VC:
                 "",
                 "",
             )
-        #person = f'{os.getenv("weight_root")}/{sid}'
-        person = sid
+        person = f'{os.getenv("weight_root")}/{sid}'
         logger.info(f"Loading: {person}")
 
         self.cpt = torch.load(person, map_location="cpu")
